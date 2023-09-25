f701c26e6ba12075b8629f3d26a94ce55cc9a29c
t <t>
2023-09-25 13:51:22 +0300

    added health

--- myapp/main.py
+++ myapp/main.py
@@ -8,6 +8,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World2"}
 
+@app.get("/heath")
+async def heath():
+    return {"status":"Healthy"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
